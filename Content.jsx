807ca2e6189953a45c3467977bde0bad7650fa4e
tictools <jaumevillarreal@gmail.com>
2024-02-05 18:19:12 +0100

    feat: define components

---
+++ Content.jsx
@@ -0,0 +1,18 @@
+import { Part } from "./Part";
+
+export const Content = ({
+  part1,
+  part2,
+  part3,
+  exercises1,
+  exercises2,
+  exercises3,
+}) => {
+  return (
+    <>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </>
+  );
+};
