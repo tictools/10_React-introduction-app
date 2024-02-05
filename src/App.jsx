bfd6a927f6bc6b3adc3447eb5d31703639f8ac1a
tictools <jaumevillarreal@gmail.com>
2024-02-05 20:13:42 +0100

    feat: implement refactor

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,37 @@ import { Total } from "../Total";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+
+  const part_01 = {
+    part: "Fundamentals of React",
+    exercises: 10,
+  };
+
+  const part_02 = {
+    part: "Using props to pass data",
+    exercises: 7,
+  };
+
+  const part_03 = {
+    part: "State of a component",
+    exercises: 14,
+  };
+
+  const part_04 = {
+    part: "State of a component",
+    exercises: 14000,
+  };
+
+  const courseData = [part_01, part_02, part_03, part_04];
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Content courseData={courseData} />
       <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        exercises1={part_01.exercises}
+        exercises2={part_02.exercises}
+        exercises3={part_03.exercises}
       />
     </div>
   );
